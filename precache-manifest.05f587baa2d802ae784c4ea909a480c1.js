b18c809a22034cf3e16b2ce058503058b46b5d46
sreskala <sam.sagas@hotmail.com>
2020-11-11 19:43:50 -0500

    Updates

---
+++ precache-manifest.05f587baa2d802ae784c4ea909a480c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41726a81d59de7e3cf51a45b2f2006ba",
+    "url": "/weather-api-app/index.html"
+  },
+  {
+    "revision": "c8ea3bc5bf1e8320144e",
+    "url": "/weather-api-app/static/css/main.d833fcf0.chunk.css"
+  },
+  {
+    "revision": "6086c6285d807a5f90d1",
+    "url": "/weather-api-app/static/js/2.471382d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-api-app/static/js/2.471382d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ea3bc5bf1e8320144e",
+    "url": "/weather-api-app/static/js/main.69851b8b.chunk.js"
+  },
+  {
+    "revision": "e560ae3e7196f4c7b937",
+    "url": "/weather-api-app/static/js/runtime-main.23225113.js"
+  },
+  {
+    "revision": "a3f33b07d83e354fae139f46f4a9174a",
+    "url": "/weather-api-app/static/media/blue-cloudy-daylight-background-weather-design_33099-512.a3f33b07.jpg"
+  }
+]);
